c7209e62bd7058788a2f6a929b316608bdfb26ce
Binnet Barry <mamadoubinnetbarry@yahoo.fr>
2022-06-05 18:31:55 +0200

    Updates

--- precache-manifest.a6faa043319f2794f5e3bc6e77d50383.js
+++ precache-manifest.a6faa043319f2794f5e3bc6e77d50383.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2b0534a35fd1e4e98ed3a85f0fbe2ec3",
+    "revision": "a6031dc72a11c07cf55ef9929eee92d0",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "24bb68ec47cfda7fd225",
-    "url": "/portfolio/static/css/main.f7354ba7.chunk.css"
+    "revision": "c436aff520e8529b4645",
+    "url": "/portfolio/static/css/main.c18239ca.chunk.css"
   },
   {
     "revision": "f90f5046add199a90e6c",
@@ -16,12 +16,20 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/3.9f01db21.chunk.js"
   },
   {
-    "revision": "24bb68ec47cfda7fd225",
+    "revision": "c436aff520e8529b4645",
     "url": "/portfolio/static/js/main.e5c111fe.chunk.js"
   },
   {
     "revision": "dc4c4e3bab6c7f69d569",
     "url": "/portfolio/static/js/runtime~main.cd79d7bc.js"
+  },
+  {
+    "revision": "bd0d7f87bb1d31b9490598c7b8b1412c",
+    "url": "/portfolio/static/media/map.bd0d7f87.jpg"
+  },
+  {
+    "revision": "ac9959458f2154578f55407715fa33b2",
+    "url": "/portfolio/static/media/shape.ac995945.svg"
   },
   {
     "revision": "f44b198757e1bcf7d1aa38c0b811765a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.94936122447ef6e48e03cbabe2a7b224.js"
+  "/portfolio/precache-manifest.a6faa043319f2794f5e3bc6e77d50383.js"
 );
 
 self.addEventListener('message', (event) => {
