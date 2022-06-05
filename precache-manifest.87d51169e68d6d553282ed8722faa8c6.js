9e2f84c45d55d951268505ae5c513bf8dc77844a
Binnet Barry <mamadoubinnetbarry@yahoo.fr>
2022-06-05 17:07:13 +0200

    Updates

---
+++ precache-manifest.87d51169e68d6d553282ed8722faa8c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d45ced5ad1e3f3fe05676ca3baf2b2c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "82d08cb0fc3c16427091",
+    "url": "/portfolio/static/css/main.14c4ced4.chunk.css"
+  },
+  {
+    "revision": "f90f5046add199a90e6c",
+    "url": "/portfolio/static/js/2.42dd7f8f.chunk.js"
+  },
+  {
+    "revision": "a9807912173ef280ea3f",
+    "url": "/portfolio/static/js/3.9f01db21.chunk.js"
+  },
+  {
+    "revision": "82d08cb0fc3c16427091",
+    "url": "/portfolio/static/js/main.90ed11d7.chunk.js"
+  },
+  {
+    "revision": "dc4c4e3bab6c7f69d569",
+    "url": "/portfolio/static/js/runtime~main.cd79d7bc.js"
+  }
+]);
